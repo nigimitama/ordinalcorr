819cb7e09151460671bc35c40816c11ca187215d
nigimitama <nigimitama@users.noreply.github.com>
2025-04-13 05:07:40 +0000

    deploy: 4269c30c9e838c1a9a2aaa2e5c23c698b2001c15

--- ordinalcorr/validation.py
+++
@@ -1,9 +0,0 @@
-class ValidationError(Exception):
-    pass
-
-
-def check_if_zero_variance(x):
-    if len(set(x)) == 1:
-        raise ValidationError(
-            "all elements in the input are the same and zero variance"
-        )
