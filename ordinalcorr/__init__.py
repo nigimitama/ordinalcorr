5c3cec7c799944a13380e7b3c2efa6b09bd01b84
nigimitama <nigimitama16@gmail.com>
2025-04-14 22:35:36 +0900

    docs: update version

--- ordinalcorr/__init__.py
+++ ordinalcorr/__init__.py
@@ -2,7 +2,7 @@
 ordinalcorr - A Python package for ordinal correlation analysis
 """
 
-__version__ = "0.4.1"
+__version__ = "0.4.2"
 
 from ordinalcorr.polytomous import polychoric_corr, polyserial_corr
 from ordinalcorr.dichotomous import biserial_corr, point_biserial_corr, tetrachoric_corr
