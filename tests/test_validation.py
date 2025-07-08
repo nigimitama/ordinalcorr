416046d684a441419b937bd419c75d07ffb4d575
nigimitama <nigimitama16@gmail.com>
2025-07-08 21:38:29 +0900

    refactor: use pytest instead of unittest for the testing package

--- tests/test_validation.py
+++ tests/test_validation.py
@@ -1,24 +1,18 @@
-import unittest
 import numpy as np
+import pytest
 from ordinalcorr.validation import (
     ValidationError,
     check_if_zero_variance,
-)  # Assuming this package is installed
+)
 
 
-class TestValidation(unittest.TestCase):
+def test_check_if_zero_variance():
     """test helper functions in polytomous"""
+    # expected passing the check
+    x = np.repeat([2, 4, 6], 10)
+    check_if_zero_variance(x)
 
-    def test_check_if_zero_variance(self):
-        # expected passing the check
-        x = np.repeat([2, 4, 6], 10)
+    # expected error
+    x = np.repeat([42], 10)
+    with pytest.raises(ValidationError):
         check_if_zero_variance(x)
-
-        # expected error
-        x = np.repeat([42], 10)
-        with self.assertRaises(ValidationError):
-            check_if_zero_variance(x)
-
-
-if __name__ == "__main__":
-    unittest.main()
